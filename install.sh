5c9ffd09a0aa5d0561ec398ff891fcefa8724fda
Ma957nu <perezmanivesamanuel@gmail.com>
2026-07-14 15:32:26 +0200

    Añadida dependencia libxcb-xfixes0-dev al instalador

--- install.sh
+++ install.sh
@@ -15,7 +15,8 @@ libxcb-cursor-dev libxcb-xtest0-dev libxcb-xinerama0-dev libxcb-keysyms1-dev \
 libxcb-icccm4-dev libxcb-randr0-dev libxcb-shape0-dev libxcb-xkb-dev \
 libxcb-xrm-dev libxkbcommon-dev libxkbcommon-x11-dev libstartup-notification0-dev \
 libgdk-pixbuf-2.0-dev libglib2.0-dev libdbus-1-dev libxdg-basedir-dev \
-libpango1.0-dev gettext -y
+libpango1.0-dev gettext libxcb-xfixes0-dev -y
+
 
 echo -e "${AZUL}[*] Compilando e instalando la última versión de AwesomeWM (Git)...${RESET}"
 if [ -d "/tmp/awesome-git" ]; then
